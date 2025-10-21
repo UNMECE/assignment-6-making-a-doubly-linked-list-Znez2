84504a4e7012351df3872ae1d44fad67cdd9bca6
Zilong <znez2@unm.edu>
2025-10-20 20:46:38 -0600

    kay it doesnt dump fault i think anymore
    orwhatever the error is

--- coordinate.h
+++ coordinate.h
@@ -2,19 +2,20 @@
 #define COORDINATE_H
 struct _coordinate
 {
-  float x;
-  float y;
-  int coord_id;
-  struct _coordinate *next;
-  struct _coordinate *previous;
+    float x;
+    float y;
+    int coord_id;
+    struct _coordinate *next;
+    struct _coordinate *previous;
+    
 };
 typedef struct _coordinate Coordinate;
 
 void add_coordinate(Coordinate *list_end, float x, float y);
 void forward_display(Coordinate *list_beginning);
 void backward_display(Coordinate *list_end);
-void delete_coordinate(Coordinate **list_beginning, int coord_id_to_delete);
+void delete_coordinate(Coordinate **list_beginning, int coord_id_somethingsomething);
 int list_length(Coordinate *list_beginning);
 void closest_to(Coordinate *list_beginning, float x, float y);
-
 #endif
+//idk like functions and crap i am not sure what i am doing but i am doing something and this seems right
--- main.cpp
+++ main.cpp
@@ -5,45 +5,35 @@
 #include "math.h"
 
 void add_coordinate(Coordinate *list_end, float x, float y){
-     if (list_end == NULL) return;
-
-    Coordinate *newNode = new Coordinate;
-    newNode->x = x;
-    newNode->y = y;
-    newNode->next = NULL;
-    newNode->previous = list_end;
-    newNode->coord_id = list_length(list_end) + 1;
-
-    list_end->next = newNode;
+    
+    Coordinate *new_nodeorsumcrap = new Coordinate;
+    new_nodeorsumcrap->x = x;
+    new_nodeorsumcrap->y = y;
+    new_nodeorsumcrap->next = NULL;
+    new_nodeorsumcrap->previous = list_end;
+    new_nodeorsumcrap->coord_id = list_length(list_end) + 1;
+    list_end->next = new_nodeorsumcrap;
+    
+    
+    
 }
 
 void forward_display(Coordinate *list_beginning){
     Coordinate *temp = list_beginning;
-    std::cout << "Forward list:" << std::endl;
-    while (temp != NULL) {
-        std::cout << "ID " << temp->coord_id << ": (" << temp->x << ", " << temp->y << ")" << std::endl;
+    std::cout << "list reading forward yay "<< std::endl;
+    while (temp != NULL){
+        std::cout << temp->x<<", "<<temp->y<<std::endl;
         temp = temp->next;
     }
-    
 }
 
-void backward_display(Coordinate *list_end){
-    Coordinate *temp = list_end;
-    std::cout << "Backward list:" << std::endl;
-    while (temp != NULL) {
-        std::cout << "ID " << temp->coord_id << ": (" << temp->x << ", " << temp->y << ")" << std::endl;
-        temp = temp->previous;
-    }
-    
-}
-
-void delete_coordinate(Coordinate** list_beginning, int coord_id_to_delete){
-     Coordinate* temp = *list_beginning;
-    while (temp != NULL && temp->coord_id != coord_id_to_delete)
+void delete_coordinate(Coordinate** list_beginning,int coord_id_somethingsomething){
+    Coordinate* temp = *list_beginning;
+    while (temp != NULL && temp->coord_id != coord_id_somethingsomething)
         temp = temp->next;
-
-    if (temp == NULL) return;
-
+        
+    if(temp == NULL) return;
+    
     if (temp->previous != NULL)
         temp->previous->next = temp->next;
     else
@@ -57,13 +47,25 @@ void delete_coordinate(Coordinate** list_beginning, int coord_id_to_delete){
 }
 
 int list_length(Coordinate *list_beginning){
-    int count = 0;
+    int count = 0;  
+    
     Coordinate *temp = list_beginning;
     while (temp != NULL) {
         count++;
         temp = temp->next;
     }
+    
     return count;
+}
+
+void backward_display(Coordinate *list_end){
+    Coordinate *temp = list_end;
+    std::cout << "We flippin a 180 with dis list (we goin backwardz) " << std::endl;
+    while (temp != NULL) {
+        std::cout << temp->x<<", "<<temp->y<<std::endl;
+        temp = temp->previous;
+    }
+    
 }
 
 void closest_to(Coordinate *list_beginning, float x, float y){
@@ -82,11 +84,9 @@ void closest_to(Coordinate *list_beginning, float x, float y){
         }
         temp = temp->next;
     }
-
-    std::cout << "Closest to (" << x << ", " << y << "): ID "<< closest->coord_id << " (" << closest->x << ", " << closest->y<< ") distance " << minDist << std::endl;
     
+    std::cout <<"beep boop heres da close thingy to "<< x << ", " << y <<"to something idk"<<closest->x << ", " << closest->y<< " distance " << minDist << std::endl;
 }
-
 
 int main(int argc, char **argv)
 {
@@ -96,11 +96,13 @@ int main(int argc, char **argv)
     Coordinate *head = new Coordinate;
     
     head->x = (rand() % 100) / 10.0f;
-    head->y = (rand() % 100) / 10.0f;
+     head->y = (rand() % 100) / 10.0f;
     
     head->coord_id = 1;
+    
     head->next = NULL;
-    head->previous = NULL;
+    
+     head->previous = NULL;
     
     
     Coordinate *tail = head;
@@ -111,18 +113,17 @@ int main(int argc, char **argv)
         add_coordinate(tail, x, y);
         tail = tail->next;
         
-        
-    }
+     }
     
     forward_display(head);
     backward_display(tail);
     
-    std::cout<<"deleting something I guess"<<std::endl;
+     std::cout<<"deleting something I guess"<<std::endl;
     delete_coordinate(&head, 2);
     tail = head;
     if (tail != NULL) {
     while (tail->next != NULL){
-        tail = tail->next;
+         tail = tail->next;
     }
 }
     
@@ -135,9 +136,43 @@ int main(int argc, char **argv)
         head = head->next;
         delete temp;
         
-        
     }
-        return 0;
+         return 0;
+         
     }
+
+
+//I ate something bad while writing this code please help my gas situation
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
